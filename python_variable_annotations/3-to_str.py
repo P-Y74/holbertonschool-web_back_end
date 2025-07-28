675dd828c1285bc2ea226774ff15f0c3654f273f
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-28 13:35:45 +0200

    feat: add header docstring

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""
+Module that provides a function to convert a float to its string
+representation.
+"""
+
+
 def to_str(n: float) -> str:
     """
     Convert a floating-point number to its string representation.
