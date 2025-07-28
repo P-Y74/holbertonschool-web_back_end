9343d5c7f2ca76198837d172ad5dcc250edc12bf
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-28 13:11:52 +0200

    feat: add documentation

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 def floor(n: float) -> int:
+    """
+    Convert a floating-point number to its floor integer value.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        int: The floor of the number as an integer.
+    """
     return int(n)
