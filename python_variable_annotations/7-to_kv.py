8cab397a927896512cf90deea4a4e1cefff1ee68
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-28 13:43:44 +0200

    fix: delete float return

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -20,4 +20,4 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
         Tuple[str, float]: A tuple where the first element is the string `k`,
         and the second element is the square of `v` as a float.
     """
-    return (k, float(v ** 2))
+    return (k, (v ** 2))
