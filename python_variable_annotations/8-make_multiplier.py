40502bea7c2a055748642f76b8bc975f502118e4
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-28 13:17:00 +0200

    feat: add documentation

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -3,6 +3,17 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Create a multiplier function that multiplies a float by a given multiplier.
+
+    Args:
+        multiplier (float): The multiplier factor.
+
+    Returns:
+        Callable[[float], float]: A function that takes a float and returns
+        the product of that float and the multiplier.
+    """
     def inner(x: float) -> float:
+        """Multiply input x by the multiplier."""
         return x * multiplier
     return inner
