d18ddcd18494dbca2307d980effbad0cddf3fdec
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-28 13:13:31 +0200

    feat: add documentation

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""
+Module that defines and annotates several basic variables.
+
+Variables:
+    a (int): An integer with value 1.
+    pi (float): A floating-point number representing Pi.
+    i_understand_annotations (bool): A boolean indicating understanding
+    of annotations.
+    school (str): A string representing the school name.
+"""
+
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
