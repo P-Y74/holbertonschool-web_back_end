4f0a311fba4bf05e69d4f0ffd2fbf1e9131b664a
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-28 13:12:28 +0200

    feat: add documentation

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
 def to_str(n: float) -> str:
+    """
+    Convert a floating-point number to its string representation.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: The string representation of the number.
+    """
     return str(n)
