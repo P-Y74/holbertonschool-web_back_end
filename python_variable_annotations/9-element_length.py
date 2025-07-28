8040a2b473f7c79b5e1e1524a4e9488761b6ca04
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-28 13:27:56 +0200

    style: add correct type annotations to element_length function

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from typing import List, Tuple, Iterable, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return a list of tuples where each tuple contains an element from
+    the input iterable and its length.
+
+    Args:
+        lst (Iterable[Sequence]): An iterable of sequences
+        (like strings, lists, etc).
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples with each sequence
+        and its length.
+    """
+    return [(i, len(i)) for i in lst]
