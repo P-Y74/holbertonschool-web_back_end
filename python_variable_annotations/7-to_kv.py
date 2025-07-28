1eef280d005874405d552898bf89f696738c9176
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-07-28 13:15:37 +0200

    feat: add documentation

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,4 +3,15 @@ from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Create a tuple with a string and the square of a number (as a float).
+
+    Args:
+        k (str): A string key.
+        v (Union[int, float]): A value to be squared, either an integer or a float.
+
+    Returns:
+        Tuple[str, float]: A tuple where the first element is the string `k`,
+        and the second element is the square of `v` as a float.
+    """
     return (k, float(v ** 2))
