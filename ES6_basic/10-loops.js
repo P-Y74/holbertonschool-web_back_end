4723f8a8c8342665d38c11cf0dba49017d6ff62f
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-08-18 15:33:23 +0200

    chore: correct function body

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   let newArray = []
   for (let value of array) {
-    let modifiedValue = appendString + value
-    newArray.push(modifiedValue)
+    newArray.push(appendString + value)
   }
 
   return newArray;
