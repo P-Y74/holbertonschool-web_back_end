9cf18644fa1a5e5ea5417a177ae07a93995481c9
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-08-20 09:00:07 +0200

    fix(building): ensure subclass must override evacuationWarningMessage using constructor.prototype

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,14 @@
 export default class Building {
-  constructor(sqft) {
-    if (typeof (sqft) !== "number") throw new TypeError('sqft must be a number');
-    this._sqft = sqft;
+    constructor(sqft) {
+        if (typeof (sqft) !== "number") throw new TypeError('sqft must be a number');
+        this._sqft = sqft;
 
-    if (this.constructor !== Building)
-      if (this.evacuationWarningMessage === undefined) throw new Error("Class extending Building must override evacuationWarningMessage");
-  }
+        if (this.constructor !== Building)
+            if (this.constructor.prototype.evacuationWarningMessage === undefined) throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
 
-  get sqft() {
-    return this._sqft;
-  }
+    get sqft() {
+        return this._sqft;
+    }
 
 }
