50a017360c56c60f688445d7db931e2cb8717c99
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-08-25 15:41:50 +0200

    feat: add createInt8TypedArray to generate a DataView over ArrayBuffer

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,7 @@
+export default function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const data = new DataView(buffer);
+  if (position >= length || position < 0) throw new Error('Position outside range');
+  data.setInt8(position, value);
+  return data;
+}
