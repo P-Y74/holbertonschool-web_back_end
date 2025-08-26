c0eeb3e63775fb12a0e06cde84352dc801c592d3
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-08-26 15:29:50 +0200

    feat: add toString to define reason as string

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,6 +4,6 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then(result =>
-      result.map(r => r.status === 'fulfilled' ? r : { status: r.status, value: r.reason })
+      result.map(r => r.status === 'fulfilled' ? r : { status: r.status, value: r.reason.toString() })
     );
 }
