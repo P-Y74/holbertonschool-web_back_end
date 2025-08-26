75889bd115ca277464cd7e562730300b22a32c8a
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-08-26 08:51:39 +0200

    feat: return a set of value with specific string

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,4 @@
+export default function cleanSet(set, startString) {
+    if (!startString) return '';
+    return [...set].filter(value => value.startsWith(startString)).map(value => value.slice(startString.length)).join('-');
+}
