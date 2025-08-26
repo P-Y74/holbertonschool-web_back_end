fd9427ef672c69fa4cf3f7e394c2d7f7fe4bd911
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-08-26 15:27:33 +0200

    feat(promise): verify promise status after resolution

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,5 +2,8 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then(result =>
+      result.map(r => r.status === 'fulfilled' ? r : { status: r.status, value: r.reason })
+    );
 }
