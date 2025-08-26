593191fcc7ce450f16a4e03c51a466241f5d3fe6
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-08-26 09:14:38 +0200

    feat: add condition to check if not a string or empty

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,6 @@
 export default function cleanSet(set, startString) {
-    if (!startString) return '';
+    if (typeof startString !== 'string' || startString.length === 0) {
+        return '';
+    }
     return [...set].filter(value => value.startsWith(startString)).map(value => value.slice(startString.length)).join('-');
 }
