a39b4da47fcc815894d5f95c0df943bd23514c27
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-09-01 11:50:59 +0200

    feat: add setEncoding

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,5 @@
 console.log('Welcome to Holberton School, what is your name?')
+process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`)
