6a3dbeddfb2e11b6177ef76f927adc44e9a87cb8
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-09-01 12:00:49 +0200

    feat: add missing semicolon

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,9 @@
-console.log('Welcome to Holberton School, what is your name?')
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
-  process.stdout.write(`Your name is: ${name}\n`)
+  process.stdout.write(`Your name is: ${name}\n`);
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-})
+});
