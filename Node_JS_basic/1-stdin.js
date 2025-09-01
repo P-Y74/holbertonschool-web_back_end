d3d91a9b750830a01c61602d0f4944dc5880faed
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-09-01 11:46:21 +0200

    fix: correct with litteral string and delete SIGINT

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,8 @@
 console.log('Welcome to Holberton School, what is your name?')
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write('Your name is: ')
-  console.log(`${name}`);
+  process.stdout.write(`Your name is: ${name}\n`)
 });
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-})
-process.on('SIGINT', () => {
-  console.log('This important software is now closing');
-  process.exit();
 })
