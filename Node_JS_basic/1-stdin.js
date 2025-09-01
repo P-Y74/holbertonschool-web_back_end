5b330320b6549afdc0e8de3772aeee94cb49b862
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-09-01 11:57:50 +0200

    fix: delete toString

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?')
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+  const name = data.trim();
   process.stdout.write(`Your name is: ${name}\n`)
 });
 process.stdin.on('end', () => {
