d2859c3b16e0a3462bcd578e80313805c9070400
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-09-02 12:35:35 +0200

    style: correct with npx eslint

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -2,25 +2,22 @@ const http = require('http');
 const url = require('url');
 const countStudents = require('./3-read_file_async');
 
-
 const app = http.createServer(async (req, res) => {
-  const reqUrl = url.parse(req.url).pathname
-  if (reqUrl == '/') {
+  const reqUrl = url.parse(req.url).pathname;
+  if (reqUrl === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
-  } else if (reqUrl == '/students') {
+  } else if (reqUrl === '/students') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     const path = process.argv[2];
     let output = '';
     const originalLog = console.log;
-    console.log = (msg) => { output += msg + '\n' };
+    console.log = (msg) => { output += `${msg}\n`; };
     await countStudents(path);
     console.log = originalLog;
-    if (output.endsWith('\n')) {
-      output = output.slice(0, -1);
-    }
-    res.end('This is the list of our students\n' + output);
+    output = output.trimEnd();
+    res.end(`This is the list of our students\n${output}`);
   }
-})
+});
 app.listen(1245);
 module.exports = app;
