e48fd1a59a30c7ac635eb6d2a6b9ff6ee01d41f3
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-09-02 11:12:42 +0200

    fix: correct with wrtiteHead, content-type

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,8 @@
-const http = require('http')
+const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
-})
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
 app.listen(1245);
+module.exports = app;
