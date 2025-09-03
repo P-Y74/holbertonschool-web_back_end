1cc03ade5422907b71907bd91e7919158682cdcb
P-Y74 <197254317+P-Y74@users.noreply.github.com>
2025-09-03 11:00:58 +0200

    feat(server): implement Express HTTP server with / and /students routes

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const countStudents = require('./3-read_file_async');
+
+app.get('/', (req, res) => {
+    res.type('text');
+    res.send('Hello Holberton School!')
+})
+app.get('/students', async (req, res) => {
+    res.type('text');
+    const path = process.argv[2];
+    let output = '';
+    const originalLog = console.log;
+    console.log = (msg) => { output += `${msg}\n`; };
+    try {
+        await countStudents(path);
+        output = output.trimEnd();
+        res.send(`This is the list of our students\n${output}`);
+        return;
+    } catch (err) {
+        console.log = originalLog;
+        res.send('This is the list of our students\nCannot load the database');
+        return;
+    } finally {
+        console.log = originalLog;
+    }
+})
+app.listen(1245)
+module.exports = app
